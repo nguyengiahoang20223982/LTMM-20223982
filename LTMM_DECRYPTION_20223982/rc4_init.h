3684a66bebb2025f676518d65052724e59abdf11
nguyengiahoang20223982 <Hoang.NG223982@sis.hust.edu.vn>
2025-10-19 22:41:00 +0700

    delete 1 bai de tao repo moi

--- LTMM_DECRYPTION_20223982/rc4_init.h
+++
@@ -1,9 +0,0 @@
-#ifndef RC4_INIT_H
-#define RC4_INIT_H
-
-void init_S(int S[], int length_s);
-void init_T(int T[], int length_s, int K[], int length_k);
-void swap(int *a, int *b);
-void key_scheduling(int S[], int T[], int length_s);
-
-#endif
